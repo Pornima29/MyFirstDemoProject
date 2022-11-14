40ba2af5c517a8f9cee84d91e32441437ea2188d
Pornima Kanchangire <shalinikanchangire1@gmail.com>
2022-11-14 15:27:35 +0530

    My First Commit code

---
+++ Main_First_Project/src/com/pornima/first/gitproject/Example.java
@@ -0,0 +1,16 @@
+package com.pornima.first.gitproject;
+
+import java.util.ArrayList;
+
+public class Example {
+
+	public static void main(String[] args) {
+		ArrayList ar = new ArrayList();
+		ar.add(12);
+		ar.add(45);
+		ar.add("Shalini");
+		System.out.println(ar);
+		
+	}
+
+}
